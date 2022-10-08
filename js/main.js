8b62bf65638f10fb1e39c53233d93acabfa8f7ed
Anur85 <an-nv85@yandex.ru>
2022-10-08 15:28:41 +0500

    fix comments

--- js/main.js
+++ js/main.js
@@ -1,35 +1,24 @@
 // подсмотрел на https://learn.javascript.ru/task/random-int-min-max
+const randomInteger = (min, max) =>
+  Math.round(min - 0.5 + Math.random() * (max - min + 1));
 
-function getRandomNumber(firstNumder, secondNumder) {
+const getRandomNumber = (firstNumder, secondNumder) => {
   if (firstNumder < 0 || secondNumder < 0) {
     return NaN;
   }
   if (firstNumder > secondNumder) {
-    const max = firstNumder;
-    const min = secondNumder;
-    const rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+    return randomInteger(secondNumder, firstNumder);
   } else {
-    const rand = firstNumder + Math.random() * (secondNumder + 1 - firstNumder);
-    return Math.floor(rand);
+    return randomInteger(firstNumder, secondNumder);
   }
-}
+};
 
-function cheсkMaxLengthStr(str, maxLength) {
-  const currentLength = str.length;
-  if (maxLength >= currentLength) {
+const isMaxLengthStr = (str, maxLength) => {
+  if (maxLength >= str.length) {
     return true;
   } else {
     return false;
   }
-} // Результат: true, если строка проходит по длине, и false — если не проходит
-
-// console.log(getRandomNumber(-20, -60));
-// console.log(getRandomNumber(1, -6));
-// console.log(getRandomNumber(-1, 6));
-// console.log(getRandomNumber(0, 6));
-// console.log(getRandomNumber(5, 0));
-// console.log(cheсkMaxLengthStr("sdsd", 4));
-// console.log(cheсkMaxLengthStr("sdsd", 3));
+};
 getRandomNumber(-20, -60);
-cheсkMaxLengthStr('sdsd', 3);
+isMaxLengthStr('sdsd', 3);
