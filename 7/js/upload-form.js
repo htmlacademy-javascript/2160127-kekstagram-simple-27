d563760feedef6c8dc12ef1efec9a5013ca026dc
Keks <keks@htmlacademy.ru>
2022-11-07 16:45:31 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/upload-form.js
+++ 7/js/upload-form.js
@@ -1,49 +1,41 @@
-import { isEscapeKey, isEnterKey } from './util.js';
+import { isEscapeKey } from './util.js';
 const uploadFile = document.querySelector('#upload-file');
 const closeButton = document.querySelector('#upload-cancel');
 
 const popupForm = document.querySelector('.img-upload__overlay');
-const body = document.querySelector('body');
+const body = document.body;
 
 const pristine = new Pristine(popupForm, {
   classTo: 'img-upload__text',
   errorTextParent: 'img-upload__text',
   errorTextClass: 'img-upload__text__error-text'
 });
-popupForm.addEventListener('submit', () => {
+
+body.addEventListener('submit', (evt) => {
+  evt.preventDefault();
   pristine.validate();
 });
 
-const onPopupEscKeydown = (evt) => {
+const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     popupClose();
   }
 };
 
-function clearUploadFile() {
-  uploadFile.value = '';
-}
-
 function popupOpen() {
   popupForm.classList.remove('hidden');
   body.classList.add('modal-open');
-  document.addEventListener('keydown', onPopupEscKeydown);
+  document.addEventListener('keydown', onDocumentKeydown);
 }
 function popupClose() {
   popupForm.classList.add('hidden');
-  clearUploadFile();
-  document.removeEventListener('keydown', onPopupEscKeydown);
+  uploadFile.value = '';
+  document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 closeButton.addEventListener('click', () => {
   popupClose();
-});
-
-closeButton.addEventListener('keydown', (evt) => {
-  if (isEnterKey(evt)) {
-    popupClose();
-  }
 });
 
 uploadFile.addEventListener('change', () => {
