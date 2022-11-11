e0fc59011e19e298209c20a8166ba691f2052763
Anur85 <an-nv85@yandex.ru>
2022-11-12 00:05:18 +0500

    move pictureSection.appendChild in function

--- js/main.js
+++ js/main.js
@@ -6,9 +6,5 @@ import './img-effect.js';
 fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
   .then((response) => response.json())
   .then((images) => {
-    // eslint-disable-next-line no-debugger
-    //debugger;
     renderObject(images);
-    // eslint-disable-next-line no-console
-    console.log(images);
   });
--- js/render.js
+++ js/render.js
@@ -1,15 +1,12 @@
-//import { createArrayObjects } from './data.js';
-
-//const otherUsersPhotos = createArrayObjects();
 const pictureTempldate = document
   .querySelector('#picture')
   .content.querySelector('a');
 
 const pictureSection = document.querySelector('.pictures');
-const picturesFragment = document.createDocumentFragment();
 
 const renderObject = (otherUsersPhotos) =>
   otherUsersPhotos.forEach(({ url, description, likes, comments }) => {
+    const picturesFragment = document.createDocumentFragment();
     const pictureElem = pictureTempldate.cloneNode(true);
     const pictureImg = pictureElem.querySelector('img');
     pictureImg.src = url;
@@ -17,10 +14,7 @@ const renderObject = (otherUsersPhotos) =>
     pictureElem.querySelector('.picture__comments').textContent = likes;
     pictureElem.querySelector('.picture__likes').textContent = comments;
     picturesFragment.appendChild(pictureElem);
-    // eslint-disable-next-line no-console
-    console.log(url, description, likes, comments);
+    pictureSection.appendChild(picturesFragment);
   });
-
-pictureSection.appendChild(picturesFragment);
 
 export { renderObject };
